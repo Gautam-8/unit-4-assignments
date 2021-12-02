a784e077422d7909367089938c450fd762d1408a
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-12-02 22:15:06 +0530

    Create products.model.js

---
+++ validations/src/models/products.model.js
@@ -0,0 +1,18 @@
+const { Schema , model} = require ("mongoose");
+
+const productsSchema = new Schema ({
+
+    first_name : {type:String , required:true},
+    last_name : {type:String , required:true},
+    email: {type:String , required:true},
+    age:{type:Number , required:true},
+    gender:{type:String , required:true},
+    pincode:{type:Number , required:true}
+   
+},
+{
+    versionKey:false,
+    timestamps:true
+});
+
+module.exports = model( "validation" , productsSchema);
