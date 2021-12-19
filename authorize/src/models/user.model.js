284407d75fa35ee2c88935c299a56bd7efaef467
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-12-19 21:15:54 +0530

    Create user.model.js

---
+++ authorize/src/models/user.model.js
@@ -0,0 +1,38 @@
+const { Schema , model } = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema ({
+
+    email:{type:String , required:true , unique:true},
+    password:{ type:String  ,required:true},
+   role:{type:String , required:true}
+},
+{
+    versionKey : false,
+    timestamps : true
+}
+);
+
+userSchema.pre("save" , function (next){    //only for create and update
+
+    if(!this.isModified("password")) return next();
+    const hash = bcrypt.hashSync(this.password, 10);
+    this.password = hash;
+          return next();
+        });
+
+
+  userSchema.methods.checkPassword = function(password){
+
+    return new Promise((resolve,reject) => {
+
+        bcrypt.compare(password , this.password ,function(err,same){
+            if(err) return reject(err);
+
+            return resolve(same);
+        })
+    })
+ }
+
+module.exports = model( "user" , userSchema);
